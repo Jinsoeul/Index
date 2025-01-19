91c68687acbfaa84562d5c97fd5f94ad37fe4a93
Jinsoeul <168828859+Jinsoeul@users.noreply.github.com>
2025-01-19 18:30:29 +0800

    Add files via upload

---
+++ Teacher.js
@@ -0,0 +1,4 @@
+document.getElementById("submitBtn").addEventListener("click", function() {
+    // Perform validation or other actions here if needed
+    window.location.href = "./Scanner/Scanner.html"; // Redirects to Scanner page
+});
